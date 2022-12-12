edb627d29d0bad8b2c5a411499ab1aa262efdfc0
Brennan Shacklett <bpshacklett@gmail.com>
2022-12-12 01:03:42 -0800

    interactive.py resets

--- tests/interactive.py
+++ tests/interactive.py
@@ -33,6 +33,8 @@ def get_keyboard_action():
             return  5
         elif key_action == 'z':
             return  6
+        elif key_action == 'r':
+            return 0
         elif key_action == 'q':
             return -1
         else:
@@ -48,8 +50,10 @@ sim = gpu_rearrange_python.RearrangeSimulator(
 )
 
 actions = sim.move_action_tensor().to_torch()
+resets = sim.reset_tensor().to_torch()
 rgb_observations = sim.rgb_tensor().to_torch()
 print(actions.shape, actions.dtype)
+print(resets.shape, resets.dtype)
 print(rgb_observations.shape, rgb_observations.dtype)
 
 while True:
@@ -57,8 +61,10 @@ while True:
     torchvision.utils.save_image((rgb_observations[0].float() / 255).permute(2, 0, 1), sys.argv[3])
 
     action = get_keyboard_action()
-
-    if action == -1:
+    
+    if action == 0:
+        resets[0] = 1
+    elif action == -1:
         break
-
-    actions[0][0] = action
+    else:
+        actions[0][0] = action
